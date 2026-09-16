6b0e20af6e210840f8c03f11bfb1b92ddb35e51e
zainthaheem <zainislam526@gmail.com>
2026-09-16 11:49:23 +0500

    Create index.ts

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,19 @@
+import {category} from './category'
+import {socialLink} from './socialLink'
+import {person} from './person'
+import {place} from './place'
+import {culture} from './culture'
+import {newsPost} from './newsPost'
+import {story} from './story'
+import {siteSettings} from './siteSettings'
+
+export const schemaTypes = [
+  category,
+  socialLink,
+  person,
+  place,
+  culture,
+  newsPost,
+  story,
+  siteSettings,
+]
