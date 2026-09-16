677a719743ac65d17690c02dc7ed2bed6e903af8
zainthaheem <zainislam526@gmail.com>
2026-09-16 11:45:13 +0500

    Create place.ts

---
+++ sanity/schemaTypes/place.ts
@@ -0,0 +1,51 @@
+import {defineType, defineField} from 'sanity'
+
+export const place = defineType({
+  name: 'place',
+  title: 'Places',
+  type: 'document',
+  fields: [
+    defineField({name: 'title', title: 'Title', type: 'string'}),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {source: 'title'},
+    }),
+    defineField({
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: [{type: 'category'}],
+    }),
+    defineField({
+      name: 'coverImage',
+      title: 'Cover Picture',
+      type: 'image',
+      options: {hotspot: true},
+    }),
+    defineField({
+      name: 'gallery',
+      title: 'More Pictures',
+      type: 'array',
+      of: [{type: 'image', options: {hotspot: true}}],
+    }),
+    defineField({
+      name: 'videoUrl',
+      title: 'Video (YouTube/Vimeo link)',
+      type: 'url',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Content',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'featured',
+      title: 'Featured on Homepage',
+      type: 'boolean',
+      initialValue: false,
+    }),
+  ],
+})
