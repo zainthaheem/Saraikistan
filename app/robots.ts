9f9e40902bbecea02b15d1175493164a54e80b82
zainthaheem <zainislam526@gmail.com>
2026-09-21 07:14:18 +0500

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://saraikistan-ml2d.vercel.app/sitemap.xml',
+  }
+}
