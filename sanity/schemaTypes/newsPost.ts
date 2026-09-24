2ad9e0c386c093025ade8da2047229e4c24ce81d
zainthaheem <zainislam526@gmail.com>
2026-09-24 06:43:40 +0500

    Update newsPost.ts

--- sanity/schemaTypes/newsPost.ts
+++ sanity/schemaTypes/newsPost.ts
@@ -1,4 +1,5 @@
 import {defineType, defineField} from 'sanity'
+import MarkdownPortableTextInput from '../components/MarkdownPortableTextInput'
 
 export const newsPost = defineType({
   name: 'newsPost',
@@ -103,6 +104,19 @@ export const newsPost = defineType({
       title: 'Content',
       type: 'array',
       of: [{type: 'block'}],
+      components: {
+        input: MarkdownPortableTextInput,
+      },
+    }),
+
+    defineField({
+      name: 'bodyUrdu',
+      title: 'Content (Urdu)',
+      type: 'array',
+      of: [{type: 'block'}],
+      components: {
+        input: MarkdownPortableTextInput,
+      },
     }),
 
     defineField({
