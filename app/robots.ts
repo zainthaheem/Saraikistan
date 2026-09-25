0de7e4dc12ecf13bac8c3b16bb34addcff698cc5
zainthaheem <zainislam526@gmail.com>
2026-09-25 23:17:57 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,3 +1,4 @@
+
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
@@ -6,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://saraikistan-ml2d.vercel.app/sitemap.xml',
+    sitemap: 'https://saraikistan.org/sitemap.xml',
+    host: 'https://saraikistan.org',
   }
 }
